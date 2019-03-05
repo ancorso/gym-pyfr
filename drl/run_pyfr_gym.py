ccbfcb3b5e4cdbbf30892852b8ba2a1c6b692390
Anthony Corso <anthonycorso92@gmail.com>
2019-03-04 17:48:29 -0800

    added mpi init for prop control

--- drl/run_pyfr_gym.py
+++ drl/run_pyfr_gym.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 
 import gym
 import gym_pyfr
+
+from mpi4py import MPI
+
+MPI.init()
 env = gym.make('gym-pyfr-v0')
 env.setup(['restart', '-b', 'openmp','cylinder_visc.pyfrm', 'cyl-2d-p2-start.pyfrs', 'config.ini'])
 
