d50e63140a47d58e8baf86e46c93b1fd2a590758
Anthony Corso <anthonycorso92@gmail.com>
2019-03-05 09:46:53 -0800

    fixed incorrect policy in DQN

--- drl/run_dqn.py
+++ drl/run_dqn.py
@@ -1,6 +1,6 @@
 import gym
 import gym_pyfr
-from stable_baselines.common.policies import CnnPolicy
+from stable_baselines.deepq.policies import CnnPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import DQN
 
@@ -8,6 +8,6 @@ env = gym.make('gym-pyfr-v0', discrete = True, n=50)
 env.setup(['restart', '-b', 'cuda','cylinder_visc.pyfrm', 'cyl-2d-p2-start.pyfrs', 'config.ini'])
 env = DummyVecEnv([lambda: env])
 
-model = PPO2(CnnPolicy, env, verbose=1)
+model = DQN(CnnPolicy, env, verbose=1)
 model.learn(total_timesteps=100000)
 model.save("pyfr-model-dqn")
