4870078306b209fa1d5edec59c2fdcde79a55207
Anthony Corso <anthonycorso92@gmail.com>
2019-03-13 21:11:56 -0700

    added options to dqn

--- drl/run_dqn.py
+++ drl/run_dqn.py
@@ -7,7 +7,7 @@ from stable_baselines import DQN #Change for different policies
 from stable_baselines.bench import Monitor
 from monitor_callback import get_callback
 
-log_dir = "/usr/anthonycorso/Dropbox/dqn_log"  #Change for different policies
+log_dir = "/home/anthonycorso/Dropbox/dqn_log"  #Change for different policies
 os.makedirs(log_dir, exist_ok=True)
 
 env = gym.make('gym-pyfr-v0', discrete = True, n=50, save_dir=log_dir) # change discrete setting for different policies
@@ -15,11 +15,8 @@ env.setup(['restart', '-b', 'cuda','cylinder_visc.pyfrm', 'cyl-2d-p2-start.pyfrs
 env = Monitor(env, log_dir, allow_early_resets=True)
 # env = DummyVecEnv([lambda: env]) # uncomment for other policies
 
-model = DQN(CnnPolicy, env, verbose=1)
-model.learn(total_timesteps=1000000,
-            callback=get_callback(log_dir),
-            buffer_size = 5000,
-            prioritized_replay = True) #Dont forget to set timesteps appropriatley
+model = DQN(CnnPolicy, env, verbose=1,buffer_size = 5000, prioritized_replay = True)
+model.learn(total_timesteps=1000000,callback=get_callback(log_dir)) #Dont forget to set timesteps appropriatley
 model.save(log_dir + "/pyfr-model-dqn")
 
 obs = env.reset()
